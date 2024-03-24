a6f945c6425735e65f2780d1d03ddc67bc38c5b0
luluxiaoyu <ljy.xiaoyu@qq.com>
2024-03-24 18:04:46 +0800

    LangManager

---
+++ MSL/i18n/LangManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Resources;
+
+namespace MSL.i18n
+{
+    public class LanguageManager : INotifyPropertyChanged
+    {
+        private readonly ResourceManager _resourceManager;
+        private static readonly Lazy<LanguageManager> _lazy = new Lazy<LanguageManager>(() => new LanguageManager());
+        public static LanguageManager Instance => _lazy.Value;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private LanguageManager()
+        {
+            _resourceManager = new ResourceManager(typeof(Lang));
+        }
+
+        public string this[string name]
+        {
+            get
+            {
+                if (name == null)
+                {
+                    throw new ArgumentNullException(nameof(name));
+                }
+                return _resourceManager.GetString(name);
+            }
+        }
+
+        public void ChangeLanguage(CultureInfo cultureInfo)
+        {
+            CultureInfo.CurrentCulture = cultureInfo;
+            CultureInfo.CurrentUICulture = cultureInfo;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Item[]"));
+        }
+    }
+}
