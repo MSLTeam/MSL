9dfd2df70e219f9b83a1639eac764b71bf2fb547
Waheal <2035582067@qq.com>
2024-03-24 22:35:11 +0800

    修复“关于开服器”标题语言不会更改的bug

--- MSL/pages/About.xaml.cs
+++ MSL/pages/About.xaml.cs
@@ -11,6 +11,10 @@ namespace MSL.pages
         public About()
         {
             InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
             AppVersionLab.Content = LanguageManager.Instance["Pages_About_AboutMSL"] + string.Format("(MSLv{0})", System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());
         }
     }
