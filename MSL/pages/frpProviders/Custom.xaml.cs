e49d0b2b709c1f0ee386a1295b96096c6e362959
luluxiaoyu <ljy.xiaoyu@qq.com>
2024-05-22 22:56:13 +0800

    feat: 自定义Frp隧道的配置文件输出

--- MSL/pages/frpProviders/Custom.xaml.cs
+++ MSL/pages/frpProviders/Custom.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MSL.controls;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -37,10 +40,71 @@ namespace MSL.pages.frpProviders
 
         private void OKBtn_Click(object sender, RoutedEventArgs e)
         {
-            
+            if (EasyMode.IsChecked==true)
+            {
+                //可视化模式
+                //检测必备数据写了没，防止小傻瓜
+                if (ServerIP.Text == "" || ServerPort.Text=="" || ClientRemotePort.Text==""||ClientIP.Text==""||ClientPort.Text=="")
+                {
+                    Shows.ShowMsg(Window.GetWindow(this), "存在未填写的数据！\n请检查！", "错误！");
+                }
+                else
+                {
+                    //既然都写了，那就继续
+                    string FrpcConfig;
+                    FrpcConfig = $"[common]\r\nserver_addr = {ServerIP.Text}\r\n" +
+                        $"server_port = {ServerPort.Text}\r\n" +
+                        $"tcp_mux = {ServerTcpMux.IsChecked.ToString().ToLower()}\r\n" +
+                        $"protocol = {ServerProtocol.Text}\r\nuser = {ServerUser.Text}\r\n" +
+                        $"token = {ServerToken.Text}\r\ndns_server = {ServerDNS.Text}\r\n" +
+                        $"tls_enable = {ServerTls.IsChecked.ToString().ToLower()}\r\n" +
+                        $"[{ClientName.Text}]\r\nprivilege_mode = {ServerPrivilege.IsChecked.ToString().ToLower()}\r\n" +
+                        $"type = {ClientProtocol.Text}\r\nlocal_ip = {ClientIP.Text}\r\n" +
+                        $"local_port = {ClientPort.Text}\r\nremote_port = {ClientRemotePort.Text}\r\n" +
+                        $"use_encryption = {ClientEnc.IsChecked.ToString().ToLower()}\r\n" +
+                        $"use_compression = {ClientComp.IsChecked.ToString().ToLower()}\r\n \r\n";
+                    File.WriteAllText(@"MSL\frpc", FrpcConfig);
+                    SetFrpcPath();
+                }
+            }
+            else
+            {
+                //直接丢配置文件模式
+                File.WriteAllText(@"MSL\frpc", ConfigBox.Text);
+                SetFrpcPath();
+            }
         }
 
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        private async void SetFrpcPath()
+        {
+            if ((bool)(CustomFrpcClient.IsChecked = true))//自定义的话要导入进MSL文件夹
+            {
+                //选择文件对还款
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "exe应用程序 (*.exe)|*.exe";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.Title = "请选择您的Frpc客户端";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //文件路径
+                    string filePath = openFileDialog.FileName;
+                    File.Copy(filePath, @"MSL/frpc_custom.exe");
+                    Config.Write("frpcServer", "-2");//自定义模式-自定义frpc客户端
+                }
+                else
+                {
+                    Config.Write("frpcServer", "-1");//自定义模式-Gh上的客户端
+                }
+
+            }
+            //最后结束
+            await Shows.ShowMsgDialogAsync(Window.GetWindow(this), "隧道配置成功，请您点击“启动内网映射”以启动映射！", "信息");
+            Window.GetWindow(this).Close();
+        }
+
+        private void EasyMode_Checked(object sender, RoutedEventArgs e)
         {
             if (init)
             {
@@ -50,7 +114,7 @@ namespace MSL.pages.frpProviders
            
         }
 
-        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        private void CustomMode_Checked(object sender, RoutedEventArgs e)
         {
             if (init)
             {
@@ -63,6 +127,7 @@ namespace MSL.pages.frpProviders
 
     }
 }
+// 最后是creeper镇楼⠀⠀
 //⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠤⠚⠉⢉⡑⠤⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 //⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠤⠊⠉⠠⣄⡀⠈⠓⢤⣀⠤⠈⠑⠢⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 //⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠤⠒⣉⠀⠀⠐⠀⠀⠀⠉⠐⠦⣉⠈⠁⠒⠤⣀⠀⠉⠐⠢⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -114,5 +179,4 @@ namespace MSL.pages.frpProviders
 //⠀⠀⠀⠀⠉⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣄⡀⠀⢼⠀⠀⠀⠀⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 //⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢀⣼⠔⠊⡇⠀⣀⣀⠼⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 //⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣿⣿⣿⣿⣿⣶⣿⡇⠀⣠⠧⠚⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣿⣿⠿⠓⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-// 最后是creeper镇楼⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣿⣿⠿⠓⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
