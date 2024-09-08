041817697f43183ca91044358461f7ab83144bca
luluxiaoyu <ljy.xiaoyu@qq.com>
2024-09-08 23:48:58 +0800

    feat: sakurafrp output config

--- MSL/pages/frpProviders/SakuraFrp.xaml.cs
+++ MSL/pages/frpProviders/SakuraFrp.xaml.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http.Headers;
@@ -27,7 +28,6 @@ namespace MSL.pages.frpProviders
     public partial class SakuraFrp : Page
     {
         string ApiUrl = "https://api.natfrp.com/v4";
-        string SakuraToken = null;
 
         public SakuraFrp()
         {
@@ -93,13 +93,14 @@ namespace MSL.pages.frpProviders
         //隧道相关
         internal class TunnelInfo
         {
-            public string ID { get; set; }
-            public string Type { get; set; }
+            public int ID { get; set; }
             public string LPort { get; set; }
             public string RPort { get; set; }
             public string LIP { get; set; }
             public string Name { get; set; }
             public string Node { get; set; }
+            public bool Online { get; set; }
+            public string Token { get; set; }
         }
 
         private async void GetTunnelList(string token)
@@ -123,10 +124,12 @@ namespace MSL.pages.frpProviders
                             {
                                 Name = $"{item["name"]}", //隧道名字
                                 Node = $"{item["node"]}", //节点id
-                                ID = $"{item["id"]}", //隧道id
+                                ID = item["id"].Value<int>(), //隧道id
                                 LIP = $"{item["local_ip"]}", //本地ip
                                 LPort = $"{item["local_port"]}", //本地端口
                                 RPort = $"{item["remote"]}", //远程端口
+                                Online = (bool)item["online"], //在线吗亲
+                                Token = token //用户token
                             });
                         });
                       
@@ -156,8 +159,56 @@ namespace MSL.pages.frpProviders
                 };
                 HttpResponse res = await HttpService.PostAsync(ApiUrl + "/tunnel/config",0,body,headersAction);
                 MessageBox.Show((string)res.HttpResponseContent);
+
+                //输出配置文件
+                //我也不知道为啥这么写
+                //反正就是cv O(∩_∩)O
+                Directory.CreateDirectory("MSL\\frp");
+                int number = Functions.Frpc_GenerateRandomInt();
+                if (!File.Exists(@"MSL\frp\config.json"))
+                {
+                    File.WriteAllText(@"MSL\frp\config.json", string.Format("{{{0}}}", "\n"));
+                }
+                Directory.CreateDirectory("MSL\\frp\\" + number);
+                File.WriteAllText($"MSL\\frp\\{number}\\frpc", (string)res.HttpResponseContent);
+                JObject keyValues = new JObject()
+                {
+                    ["frpcServer"] = "3", //3在msl代表sakuarfrp
+                };
+                JObject jobject = JObject.Parse(File.ReadAllText(@"MSL\frp\config.json", Encoding.UTF8));
+                jobject.Add(number.ToString(), keyValues);
+                string convertString = Convert.ToString(jobject);
+                File.WriteAllText(@"MSL\frp\config.json", convertString, Encoding.UTF8);
+                await Shows.ShowMsgDialogAsync(Window.GetWindow(this), "映射配置成功，请您点击“启动内网映射”以启动映射！", "信息");
+                Dispatcher.Invoke(() =>
+                {
+                    Window.GetWindow(this).Close();
+                });
             }
             catch (Exception ex) { 
+            }
+        }
+
+        //显示隧道信息
+        private void FrpList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var listBox = sender as System.Windows.Controls.ListBox;
+            if (listBox.SelectedItem is TunnelInfo selectedTunnel)
+            {
+                TunnelInfo_Text.Text = $"隧道名: {selectedTunnel.Name}" +
+                    $"\n隧道ID: {selectedTunnel.ID}"+ $"\n远程端口: {selectedTunnel.RPort}" + $"\n隧道状态: {(selectedTunnel.Online? "在线" : "离线")}";
+                LocalIp.Text = selectedTunnel.LIP;
+                LocalPort.Text = selectedTunnel.LPort;
+            }
+        }
+
+        //确定 输出config
+        private void OKBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var listBox = FrpList as System.Windows.Controls.ListBox;
+            if (listBox.SelectedItem is TunnelInfo selectedTunnel)
+            {
+                Task.Run(() => GetTunnelConfig(selectedTunnel.Token,selectedTunnel.ID));
             }
         }
     }
