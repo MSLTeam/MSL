bf3ca362d078e3e0faff7b3079d40a5fe5e7b34a
luluxiaoyu <ljy.xiaoyu@qq.com>
2024-09-08 22:30:04 +0800

    feat: sakurafrp user login & user info show

--- MSL/pages/frpProviders/SakuraFrp.xaml.cs
+++ MSL/pages/frpProviders/SakuraFrp.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MSL.utils;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,64 @@ namespace MSL.pages.frpProviders
     /// </summary>
     public partial class SakuraFrp : Page
     {
+        string ApiUrl = "https://api.natfrp.com/v4";
+
         public SakuraFrp()
         {
             InitializeComponent();
+        }
+
+        private void Page_Initialized(object sender, EventArgs e)
+        {
+            //显示登录页面
+            LoginGrid.Visibility=Visibility.Visible;
+            MainGrid.Visibility=Visibility.Collapsed;
+            CreateGrid.Visibility=Visibility.Collapsed;
+        }
+
+        private async void userTokenLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string token;
+            token = await Shows.ShowInput(Window.GetWindow(this), "请输入Sakura账户Token", "", true);
+            if (token != null)
+            {
+                bool save = (bool)SaveToken.IsChecked;
+                ShowDialogs showDialogs = new ShowDialogs();
+                showDialogs.ShowTextDialog(Window.GetWindow(this), "登录中……");
+                await Task.Run(() => VerifyUserToken(token.Trim(), save)); //移除空格，防止笨蛋
+                showDialogs.CloseTextDialog();
+            }
+        }
+
+        private async void VerifyUserToken(string token, bool save)
+        {
+            try
+            {
+                HttpResponse res = await HttpService.GetAsync(ApiUrl+"/user/info?token="+token);
+                if (res.HttpResponseCode == System.Net.HttpStatusCode.OK)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        //显示main页面
+                        LoginGrid.Visibility = Visibility.Collapsed; ;
+                        MainGrid.Visibility = Visibility.Visible;
+                        CreateGrid.Visibility = Visibility.Collapsed;
+                    });
+                    JObject JsonUserInfo = JObject.Parse((string)res.HttpResponseContent);
+                    Dispatcher.Invoke(() =>
+                    {
+                        UserInfo.Text = $"用户名: {JsonUserInfo["name"]}\n用户类型: {JsonUserInfo["group"]["name"]}\n限速: {JsonUserInfo["speed"]}";
+                    });
+                }
+                else
+                {
+                    MessageBox.Show(res.HttpResponseContent.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
